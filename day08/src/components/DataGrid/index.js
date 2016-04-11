96754eb2c84446fdbaed41a180eae7e8d77ddd54
liekkas <zjq0717@163.com>
2016-04-11 15:32:28 +0800

    add DataGrid component

---
+++ day08/src/components/DataGrid/index.js
@@ -0,0 +1,116 @@
+/**
+ * Created by liekkas on 16/3/7.
+ */
+import React, { PropTypes } from 'react'
+import { Table, Pagination } from 'antd'
+import _ from 'lodash'
+import shallowCompare from 'react-addons-shallow-compare'
+
+const styles = {
+  root: {
+    position: 'relative',
+    backgroundColor: '#242930',
+    marginBottom: '15px',
+    marginRight: '15px',
+    height: '400px',
+  },
+  titleStyle: {
+    width: '100%',
+    height: '35px',
+    padding: '5px',
+    fontSize: '14px',
+//    color: '#f0ad4e',
+    color: '#E2E3E4',
+    backgroundColor: '#242930',
+  },
+  table: {
+    margin: '10px 20px 0px',
+  },
+  pagination: {
+    right: '10px',
+    float: 'right',
+  }
+}
+
+class DataGrid extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pageSize: props.pageSize,
+      current: 1,
+    }
+  }
+
+  //pageSize改变,重新回到第一页
+  onShowSizeChange(current, pageSize) {
+//    console.log('Current: ', current, '; PageSize: ', pageSize);
+    this.setState({ pageSize, current: 1 })
+  }
+
+  onChange(current) {
+    this.setState({ current })
+  }
+
+  componentWillReceiveProps(nextProps, nextState) {
+//    console.log('>>> DataGrid:componentWillReceiveProps', nextProps)
+    if (!shallowCompare(this, nextProps, nextState)) {
+      this.setState({pageSize:10, current:1})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+//    console.log('>>> DataGrid:componentDidUpdate', prevProps)
+  }
+
+  showTotal(total) {
+    return `共 ${total} 条`;
+  }
+
+  render() {
+    const { columns, datas, title } = this.props
+    const { pageSize, current } = this.state
+
+//    console.log('>>> DataGrid ', pageSize, current, columns, datas)
+
+    const start = (current - 1) * pageSize
+    const end = Math.min(current * pageSize, datas.length)
+    let renderData = []
+    for (let i = start; i < end; i++) {
+      renderData.push(datas[i])
+    }
+
+    return (
+      <div style={styles.root}>
+        <div style={styles.titleStyle}>{title} - 列表</div>
+        <Table dataSource={renderData}
+               useFixedHeader={true} rowKey={item => item.uid}
+               columns={columns} size="middle"
+               style={styles.table} pagination={false}/>
+        <div style={styles.pagination}>
+          <Pagination showSizeChanger showQuickJumper showTotal={(a) => this.showTotal(a)}
+                      current={current}
+                      onChange={(v) => this.onChange(v)} pageSizeOptions={['10','20','30','40']}
+                      onShowSizeChange={(a,b) => this.onShowSizeChange(a,b)}
+                      defaultPageSize={this.props.pageSize}
+                      defaultCurrent={1} total={datas.length} />
+        </div>
+      </div>
+    )
+  }
+}
+
+DataGrid.propTypes = {
+  columns: PropTypes.array.isRequired,
+  datas: PropTypes.array.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+}
+DataGrid.defaultProps = {
+  columns: [],
+  datas: [],
+  pageSize: 10
+}
+
+export default DataGrid
+//  ['7','10','24','30']
+// rowKey={item => item.date}
